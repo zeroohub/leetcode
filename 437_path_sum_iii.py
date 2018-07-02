256f874e63a1c315948feba3ebbd17e0b8076f9c
dorian <spamzero@yeah.net>
2018-07-02 16:07:37 +0800

    add more

--- 437_path_sum_iii.py
+++ 437_path_sum_iii.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from collections import defaultdict
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -15,22 +15,23 @@ class MySolution(object):
         """
         self.counter = 0
         self.total = total
-        self.checkSum(root, [])
+        self.checkSum(root, defaultdict(int))
         return self.counter
 
     def checkSum(self, node, current_totals):
         if not node:
             return
         else:
-            next_totals = []
-            for t in current_totals:
-                nt = t + node.val
-                next_totals.append(nt)
-                if nt == self.total:
-                    self.counter += 1
+            next_totals = defaultdict(int)
+            for val, cnt in current_totals.iteritems():
+                new_val = val + node.val
+                next_totals[new_val] = cnt
+                if new_val == self.total:
+                    self.counter += cnt
+
             if node.val == self.total:
                 self.counter += 1
-            next_totals.append(node.val)
+            next_totals[node.val] += 1
 
             self.checkSum(node.left, next_totals)
             self.checkSum(node.right, next_totals)
