56783293e4acad6928f43cc2625d861f04e0ceba
dorian <spamzero@yeah.net>
2018-07-05 11:53:52 +0800

    53

--- 53_maximum_subarray.py
+++ 53_maximum_subarray.py
@@ -2,32 +2,21 @@
 
 class Solution(object):
     """
-    TODO: DP算法
+    three formula:
+    f(0) = nums[0]
+    f(1) = max(nums[1], nums[1] + nums[0])
+    f(n) = max(nums[n], nums[n] + f(n-1))
     """
+
     def maxSubArray(self, nums):
         if not nums:
             return None
         if len(nums) == 1:
             return nums[0]
-
-        end = 0
-        start = 0
-        big = nums[0]
-        current = 0
-        for i, n in enumerate(nums):
-            current += n
-            if current > big:
-                big = current
-                end = i
-
-        current = big
-        for i in range(end):
-            current -= nums[i]
-            if current > big:
-                big = current
-                start = i
-
-        return big
-
-
-print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+        real_max = nums[0]
+        maximum = 0
+        for i in nums:
+            maximum = max(i, i + maximum)
+            if maximum > real_max:
+                real_max = maximum
+        return real_max
