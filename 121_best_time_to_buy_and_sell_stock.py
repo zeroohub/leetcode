5ff0cf39e3b7874439d9ec1d2b28543e4e4d72ec
dorian <spamzero@yeah.net>
2018-07-05 15:53:24 +0800

    121

--- 121_best_time_to_buy_and_sell_stock.py
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+
+        best_profit = 0
+        current_minimum = prices[0]
+
+        for i in prices:
+            current_minimum = min(i, current_minimum)
+            profit = i - current_minimum
+            if profit > best_profit:
+                best_profit = profit
+        return best_profit
+
