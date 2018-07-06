0c86f3ed9fbe21e91efba3d79ecdfbdda5accf70
Dorian Wang <spamzero@yeah.net>
2018-07-06 17:58:08 +0800

    538

---
+++ 169_majority_element.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from data_structure import *
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
--- 538_convert_bst_to_greater_tree.py
+++ 538_convert_bst_to_greater_tree.py
@@ -1,9 +1,48 @@
 # -*- coding: utf-8 -*-
 from data_structure import *
 
-class Solution(object):
+class MySolution(object):
     def convertBST(self, root):
         """
         :type root: TreeNode
         :rtype: TreeNode
         """
+        if not root:
+            return root
+        self.traversing(root, 0)
+        return root
+
+    def traversing(self, node, big_val):
+        if not node.right and not node.left:
+            node.val += big_val
+            return node.val
+        if node.right:
+            node.val += self.traversing(node.right, big_val)
+        else:
+            node.val += big_val
+
+        if node.left:
+            return self.traversing(node.left, node.val)
+
+        return node.val
+
+
+
+class Solution2(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.total = 0
+
+        def traverse(root):
+            if not root:
+                return root
+            traverse(root.right)
+            root.val += self.total
+            self.total = root.val
+            traverse(root.left)
+
+        traverse(root)
+        return root
