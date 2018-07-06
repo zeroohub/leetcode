7c7ec3f169e6062456767ef2849969c72c1ebbf2
Dorian Wang <spamzero@yeah.net>
2018-07-06 18:06:48 +0800

    169

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from data_structure import *
+from collections import defaultdict
+
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        ma = len(nums) / 2
+
+        counter = defaultdict(int)
+        for i in nums:
+            counter[i] += 1
+            if counter[i] > ma:
+                return i
+
+
