0ae5037956a1129660ca7734ef5ebf2ecaad3380
dorian <spamzero@yeah.net>
2018-07-06 15:29:39 +0800

    add more

---
+++ 538_convert_bst_to_greater_tree.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from data_structure import *
+
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
