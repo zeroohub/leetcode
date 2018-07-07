bd26dff1f8c2773ce349495034ff68c43edbca68
Dorian Wang <spamzero@yeah.net>
2018-07-07 12:55:02 +0800

    226

---
+++ 226_invert_binary_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from data_structure import *
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+    def traversing(self, node):
+        pass
