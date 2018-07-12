4fff571ef3481084f5d0adc8ebf84c470b10cac3
dorian <uulake@outlook.com>
2018-07-12 23:24:24 +0800

    287

---
+++ 287_find_the_duplicate_number.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    """
+    fail to find solution
+    TODO
+    http://keithschwarz.com/interesting/code/?dir=find-duplicate
+    https://leetcode.com/problems/find-the-duplicate-number/solution/
+    """
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
