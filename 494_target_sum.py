79509caf1e6357c4e7a22cddd9833fe788616c8f
dorian <uulake@outlook.com>
2018-07-14 22:53:01 +0800

    494

---
+++ 494_target_sum.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
