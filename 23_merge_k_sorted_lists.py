2262949398c981f389cd52f37597a28676f12c76
dorian <spamzero@yeah.net>
2018-07-31 22:23:18 +0800

    23

--- 23_merge_k_sorted_lists.py
+++ 23_merge_k_sorted_lists.py
@@ -11,19 +11,54 @@ class Solution(object):
         head = result = ListNode(None)
 
         while lists:
-            min_node = None
             min_idx = 0
-            for idx, node in enumerate(lists):
-                if (not min_node) or (node and node.val < min_node.val):
-                    min_node = node
+            for idx in range(len(lists)):
+                if not lists[idx]:
+                    continue
+                if lists[min_idx] and lists[idx].val < lists[min_idx].val:
                     min_idx = idx
 
-            result.next = lists.pop(min_idx)
-            if min_node and min_node.next:
+            min_node = lists.pop(min_idx)
+            if min_node:
+                result.next = min_node
+                result = result.next
                 lists.append(min_node.next)
-            result = result.next
 
         return head.next
 
 
-print(Solution().mergeKLists([stringToListNode('[]'), stringToListNode('[]')]))
+class Solution2(object):
+    def mergeKLists(self, lists):
+        """
+        divide and conquer
+        https://leetcode.com/problems/merge-k-sorted-lists/solution/
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next=l2
+        else:
+            point.next=l1
+        return head.next
+
+
+print(Solution().mergeKLists([stringToListNode('[]'), stringToListNode('[1]')]))
